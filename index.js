2d79d56c5e320b6218ed99d5f06187fffefe83ba
Aria Andika <andikaakun17@gmail.com>
2022-12-20 09:07:41 +0700

    trying ws

--- index.js
+++ index.js
@@ -1,97 +1,35 @@
-// @ts-checks
-import { init } from "./router.js";
-import { ServerResponse } from "http";
-import { readFile, writeFile } from "fs/promises";
+import { createServer } from 'http';
+import {  server } from "websocket";
+import { idgen } from './lib/utility.js';
 
 
-const app = init();
-const db = await readFile('./db.json').then(e=>JSON.parse(e));
-
-/** @type {ServerResponse[]} */
-const pendings = []
-
-let lastMode = '';
-let state = 0;
-
-//!================================================================
-
-// listener
-
-app.get(`/`,async (req,res)=>{
-	setcors(res)
+const http = createServer((req,res)=>{
 	
-	res.setHeader('Content-Type','text/html')
-	res.end(await readFile('pages/index.html','utf-8'));
+	
+	res.end("ok");
 })
 
-app.get('/init',(req,res)=>{
-	setcors(res);
-	res.end(`${state}`);
+const ws = new server({
+	"httpServer" : http
 })
 
-app.post('/write',(req,res)=>{
-	setcors(res);
-	write(req.body);
-	res.end('Success');
-})
 
-app.get('/listen',(req,res)=>{
-	setcors(res)
-	// pendings.push(res);
-	res.end(`${process.env['state']}|${process.env['lastState']}`)
-})
-
-app.get('/send',(req,res)=>{
-	console.log('ESP32:',req.query['msg']);
-	// pendings.forEach(e=>{
-	// 	e.end(req.query['msg'])
-	// })
-	process.env['state'] = `${parseInt(process.env['state']) + 1}`;
-	process.env['lastState'] = req.query['msg'];
-	// lastMode = req.query['msg'];
-	setcors(res)
-	res.end(`sended ${req.query['msg']}`)
-})
-
-app.get('/all',(req,res)=>{
-	setcors(res);
-	res.end(JSON.stringify(db));
-})
-
-app.listen(4040,console.log);
-
-//!================================FUNCTION================================
+/** @type {{ob: connection}} */
+const clients = {}
 
 
-function setcors(res) {
-	res.setHeader('Access-Control-Allow-Origin', '*'); /* @dev First, read about security */
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-	res.setHeader('Access-Control-Max-Age', 2592000); // 30 days
-	res.setHeader('Access-Control-Allow-Headers', '*');
-}
-
-function write(data) {
-	const bulanMap = {
-		Dec : 'Desember'
-	}
-	data.forEach(e=>{
-		delete e.img
-		delete e.desc
-		delete e.sold
+ws.on('request', req => {
+	const client = req.accept(null,req.origin);
+	const id = idgen();
+	clients[id] = client;
+	console.log("CLIENT CONNECTED",Object.keys(clients))
+	client.on('message',msg=>{
+		Object.values(clients).forEach(c=>{
+			c.send(msg.utf8Data)
+		})
 	})
-	const date = `${new Date()}`.split(' ');
-	const parsed = {
-		waktu : date[4],
-		tanggal : date[2],
-		bulan : bulanMap[date[1]],
-		tahun : date[3],
-		total : data.reduce((p,c,i)=>{
-			return {
-				price : p.price + c.price
-			}
-		}).price,
-		data
-	}
-	db.push(parsed);
-	writeFile('db.json',JSON.stringify(db));
-}
+	client.on('close',()=>{delete clients[id];console.log("CLIENT DISCONNECTED",Object.keys(clients))})
+})
+
+
+http.listen(4040,()=>console.log("Listening in 4040..."));
